3ef6fde39c944bb262625a4c89458e6d47e33e27
Kamylla <abdel.jawad987@outlook.com>
2020-08-19 22:01:55 -0300

    primeiro programa em java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{	
+	public static void main (String[] args)
+	{
+		System.out.println("Hello, world!");
+		System.out.print("Esta e uma saudacao: ");
+		System.out.printf("Idade: %d\n", 20);
+	}
+}
